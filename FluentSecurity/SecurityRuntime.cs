58774c2d67e7584188a9578df143c9e73f9edfc6
Alexander Kot <Alexander.Kot@infopulse.com>
2023-12-21 12:18:12 +0200

    Removed unneded usings

--- FluentSecurity/SecurityRuntime.cs
+++ FluentSecurity/SecurityRuntime.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+
 using FluentSecurity.Caching;
 using FluentSecurity.Configuration;
+
 using Microsoft.AspNetCore.Http;
 
 namespace FluentSecurity
